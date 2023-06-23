0802634012b24802a1abf664301fb6ca803f2ac9
Othon Gomes <othonmarcello@hotmail.com>
2023-06-23 10:25:45 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessException.java
@@ -0,0 +1,11 @@
+package chess;
+
+public class ChessException extends RuntimeException{
+    public static final long serialVersion = 1L;
+
+    public ChessException(String msg) {
+        super(msg);
+    }
+
+
+}
